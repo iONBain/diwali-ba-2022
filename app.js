a0b8d18cce722d05cd01819ba42382448078c10a
Bhaskar Agrawal <8dbdown@gmail.com>
2022-10-24 21:51:44 +0530

    d6

--- app.js
+++ app.js
@@ -118,3 +118,12 @@ let t121 = setInterval(quoteFunc, timerTime);
     //     });
     //     xhr.send();
     // });
+
+    var button = document.getElementById('button');
+    var audio = document.getElementById('audio');
+
+    var onClick = function() {
+        audio.play(); // audio will load and then play
+    };
+
+    button.addEventListener('click', onClick, false);
